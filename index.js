52b9324a97302693254c9a34f30769477ab8b76a
Rushikesh Shashikant Satpute <38781297+Rushikesh422@users.noreply.github.com>
2024-03-07 17:36:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+let counter = 0;
+function checkClicks()
+{
+  counter=counter+1;
+  var cvalue = document.getElementById("username").value;
+  console.log("Current Counter: "+ counter);
+  if (counter <= 1)
+  {
+     document.getElementById("count").innerHTML = "You have clicked the button: " + parseInt(counter)+ " Time!";
+  }
+  else
+  {
+    document.getElementById("count").innerHTML = "You have clicked the button: " + parseInt(counter)+ " Times!";
+  }
+  
+  return counter;
+} //End of CheckClicks Function
+
+    function setCookie(cookieName, cookieValue, expirationDays) 
+    {
+        console.log("Start of setCookie Method!");
+        var d = new Date();
+        d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+        console.log("End of setCookie Method!");
+    } //End of setCookie() Function
+
+function getCookie()
+{
+    //document.cookie = "username=Rushikesh Satpute";
+    let cookieList = document.cookie;
+    console.log("Start of getCookie Method!");
+    if(cookieList.length != undefined)
+    {
+        document.getElementById("getcookie").innerHTML = cookieList;
+        document.getElementById("getcookie").innerHTML = "CookiesList";
+    }
+    else
+    {
+        document.getElementById("getcookie").innerHTML = "No Cookies Are been set! :(";
+    }
+    console.log("Get Cookies List: "+ cookieList);
+    console.log("End of getCookie Method!");
+}
+
+function handleClick() {
+    setCookie("exampleCookie", "cookieValue", 30); // Set a cookie named "exampleCookie" with a value "cookieValue" that expires in 30 days
+    alert("Cookie set successfully!");
+}
